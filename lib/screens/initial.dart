7b8f56ee0084f40958fd01d3324c54d9e1d7d932
Geotryy <140184115+Geotryy@users.noreply.github.com>
2024-09-19 02:42:21 -0300

    first commit

---
+++ lib/screens/initial.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:quiz/screens/home.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('lib/assets/images/Dance.jpg'),
+                  fit: BoxFit.cover),
+            ),
+            child: Align(
+              alignment: Alignment(0.0, 0.4),
+              child: SizedBox(
+                width: 180,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => HomeScreen(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    "Entrar",
+                    style: TextStyle(color: Colors.black, fontSize: 17),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
